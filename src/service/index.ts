17a98af31f0af5af9967e377db7e4d2d9148f72a
zweirdo <1606403438@qq.com>
2023-08-20 21:06:58 +0800

    请求用户信息-请求拦截-携带token

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,11 +1,7 @@
+import { localCache } from '@/utils/cache'
 import { BASE_URL, TIME_OUT } from './config'
 import HYRequest from './request'
-
-const req = new HYRequest({
-  baseURL: BASE_URL,
-  timeout: TIME_OUT
-  // 这个请求就没有添加拦截器, 用的就是默认拦截器
-})
+import { LOGIN_TOKEN } from '@/global/constants'
 
 export const hyRequest = new HYRequest({
   // 传递特定拦截器
@@ -15,6 +11,12 @@ export const hyRequest = new HYRequest({
   // 因为原本传递的类型中没有interceptors这个属性, 我们就要针对于原本类型进行拓展
   interceptors: {
     requestSuccessFn: (config) => {
+      // 每一个请求都自动携带token
+      const token = localCache.getCache(LOGIN_TOKEN)
+      if (config.headers && token) {
+        // 类型缩小
+        config.headers.Authorization = 'Bearer' + localCache.getCache(LOGIN_TOKEN)
+      }
       return config
     },
     requestFailureFn: (err) => {
--- src/stores/login/login.ts
+++ src/stores/login/login.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import { accountLoginRequest } from '@/service/login/login'
+import { accountLoginRequest, getUserInfoById } from '@/service/login/login'
 import type { IAccount } from '@/types'
 import { localCache } from '@/utils/cache'
 import router from '@/router'
@@ -9,23 +9,27 @@ import { LOGIN_TOKEN } from '@/global/constants'
 
 const useLoginStore = defineStore('login', {
   state: () => ({
-    id: '',
-    token: localCache.getCache(LOGIN_TOKEN) ?? '',
     // 没有值就是空字符串
-    name: ''
+    token: localCache.getCache(LOGIN_TOKEN) ?? '',
+    userInfo: {}
   }),
   actions: {
     async loginAccountAction(account: IAccount) {
       // 1.账号登录, 获取token等信息
       const loginResult = await accountLoginRequest(account)
-      this.id = loginResult.data.id
-      this.name = loginResult.data.name
+      const id = loginResult.data.id
+      const name = loginResult.data.name
       this.token = loginResult.data.token
 
       // 2.进行本地缓存
       localCache.setCache(LOGIN_TOKEN, this.token)
 
-      // 3.页面跳转(main页面)
+      // 3.获取登录用户的详细信息(role信息)
+      const userInfoResult = await getUserInfoById(id)
+      const userInfo = userInfoResult.data
+      this.userInfo = userInfo
+
+      // 4.页面跳转(main页面)
       router.push('/main')
     }
   }
