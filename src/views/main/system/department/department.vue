02aac1b86dac761af1815cdefa03d94e3dc3b245
zweirdo <1606403438@qq.com>
2023-09-02 15:41:30 +0800

    将增删改查的操作抽取hook

--- src/views/main/system/department/department.vue
+++ src/views/main/system/department/department.vue
@@ -33,6 +33,8 @@ import PageModal from '@/components/page-modal/page-modal.vue'
 import searchConfig from './config/search.config'
 import contentConfig from './config/content.config'
 import modalConfig from './config/modal.config'
+import usePageContent from '@/hooks/usePageContent'
+import usePageModal from '@/hooks/usePageModal'
 
 // 对modalConfig进行操作
 // 传递有哪些上级部门
@@ -51,22 +53,10 @@ const modalConfigRef = computed(() => {
 })
 
 // 点击search, content的操作
-const contentRef = ref<InstanceType<typeof PageContent>>()
-function handleQueryClick(queryInfo: any) {
-  contentRef.value?.fetchPageListData(queryInfo)
-}
-function handleResetClick() {
-  contentRef.value?.fetchPageListData()
-}
+const { contentRef, handleQueryClick, handleResetClick } = usePageContent()
 
 // 点击content, modal的操作
-const modalRef = ref<InstanceType<typeof PageModal>>()
-function handleNewClick() {
-  modalRef.value?.setModalVisible()
-}
-function handleEditClick(itemData: any) {
-  modalRef.value?.setModalVisible(false, itemData)
-}
+const { modalRef, handleNewClick, handleEditClick } = usePageModal()
 </script>
 
 <style scoped></style>
